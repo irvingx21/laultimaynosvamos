fa5c41a344fed0a91eced367d71d15fd2165f2a9
DESKTOP-RUQICM2\Usuario <irving_69@live.com>
2016-09-05 17:43:09 -0500

    commit 2

--- src/nivel1c8/Nivel1c8.java
+++ src/nivel1c8/Nivel1c8.java
@@ -27,7 +27,7 @@ public class Nivel1c8 {
         int valor3=0;
         int menor=0;
         Scanner entrada=new Scanner(System.in);
-        
+        System.out.println("porfin salio");
         System.out.println("ingrese el primer valor ==> ");
         valor1=entrada.nextInt();
         System.out.println("ingrese el segundo valor ==> ");
